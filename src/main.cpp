d62d83cea460e338b46198fa19947fb5b4a33553
Nik <nikzeman37@gmail.com>
2026-04-02 17:31:36 +0200

    test tlacitek..moznabudem predelavat kvuli back buttons

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,36 @@
 #include <Arduino.h>
 #include "robotka.h"
 
-// Struktura pro přijímání dat o senzorech
-// Musí být stejná jako na odesílajícím ESP32
-typedef struct __attribute__((packed)) {
-    uint8_t sensor_id;
-    uint16_t distance; // mm
-} SensorData;
-
-SensorData received_data;
-
 void setup() {
     rkConfig cfg;
     rkSetup(cfg);
-    printf("Robotka prijimac UART started!\n");
-    
-    // Inicializace UART komunikace
-    rkUartInit();
-
-    printf("Cekam na data ze senzoru...\n");
+    printf("Robotka test tlacitek a LED!\n");
 }
 
 void loop() {
-    // Zkusíme přijmout data
-    if (rkUartReceive(&received_data, sizeof(received_data))) {
-        // Pokud jsme úspěšně přijali data, vypíšeme je
-        printf("Senzor ID: %d, Vzdalenost: %d mm\n", received_data.sensor_id, received_data.distance);
-        if(received_data.sensor_id == 3){
-          printf("-------------------------------------------------------- \n");
-          std::cout<<" "<<std::endl;
-        }
+    // Loop se opakuje do nekonecna
+    
+    // Pri stisknuti UP zapneme cervenou
+    if (rkButtonIsPressed(BTN_UP)) {
+        rkLedRed(true);
+    } else {
+        rkLedRed(false);
     }
 
-    // Malá pauza, abychom nezahltili procesor
+    // Dalsi tlacitka (modrou vynechavame, protoze nefunguje)
+    if (rkButtonIsPressed(BTN_DOWN)) {
+        rkLedGreen(true);
+    } else {
+        rkLedGreen(false);
+    }
+
+    if (rkButtonIsPressed(BTN_LEFT) || rkButtonIsPressed(BTN_RIGHT)) {
+        rkLedYellow(true);
+    } else {
+        rkLedYellow(false);
+    }
+
+    // Mala pauza
     delay(10); 
 }
 
